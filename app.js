9124021b1b1a164fd216f7792b890c841e014061
mprasanth113@gmail.com <mprasanth113@gmail.com>
2018-09-15 13:51:50 +0530

    03 commit

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/gg', (req, res) => {
     console.log("mmmmmmm")
     res.send('Hello World!')
 });
+app.get('/', (req, res) => {
+    console.log("ssssss")
+    res.send('Hello World!')
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
