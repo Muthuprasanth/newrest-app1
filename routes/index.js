8140a4784b0a15e43cfdee3649bb498f157a78e0
mprasanth113@gmail.com <mprasanth113@gmail.com>
2018-09-19 17:11:24 +0530

    14 commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
+var fs = require("fs");
+var path = require('path');
+var request = require('request');
+var textract = require('textract');
+var sppull = require("sppull").sppull;
+var Sendgrid = require("sendgrid-web");
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var https = require ('https');
+
 var express = require('express');
 var router = express.Router();
 
+var phrasecount = 10;
+var sendgridCredentials = [];
+
 /* GET home page. */
 router.get('/tasks', function(req, res, next) {
+  var filename = req.query.filename;
+  var filename = "mahesh_1.docx";
+  var resumedetail = "", JDdetail = "";
+  var resumekeyphrase, JDkeyphrase;
+  console.log("inside main function phrasecount is ", phrasecount);
   res.send("HEllo new app");
   //res.render('index', { title: 'Express' });
 });
